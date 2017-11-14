66cc6ae4f840679c85b6d5f833847eeb4f4d5766
sil9 <sil9tyt@gmail.com>
2017-11-14 23:31:58 +0200

    Update add and update methods to return object

--- catalog/src/main/java/com/netcracker/selyutin/dao/BaseDao.java
+++ catalog/src/main/java/com/netcracker/selyutin/dao/BaseDao.java
@@ -6,9 +6,9 @@ import java.util.List;
 
 public interface BaseDao<T extends IdentifiedEntity> {
 
-    void add(T entity);
+    T add(T entity);
 
-    void update(T entity);
+    T update(T entity);
 
     void delete(T entity);
 
