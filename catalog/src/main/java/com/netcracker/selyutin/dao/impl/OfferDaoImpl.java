892f2740c1b1aca28759e25f8e3b169796bcf011
sil9 <sil9tyt@gmail.com>
2017-11-28 00:02:37 +0200

    create typed query

--- catalog/src/main/java/com/netcracker/selyutin/dao/impl/OfferDaoImpl.java
+++ catalog/src/main/java/com/netcracker/selyutin/dao/impl/OfferDaoImpl.java
@@ -7,7 +7,8 @@ import com.netcracker.selyutin.entity.Offer;
 import com.netcracker.selyutin.entity.Tag;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -19,22 +20,40 @@ public class OfferDaoImpl extends GenericDao<Offer> implements OfferDao {
 
     @Override
     public List<Offer> findByTag(Tag tag) {
-        Query query = entityManager.createQuery(DatabaseQuery.FIND_OFFERS_BY_TAG);
+        List<Offer> offers = new ArrayList<>();
+        TypedQuery<Offer> query = entityManager.createQuery(DatabaseQuery.FIND_OFFERS_BY_TAG, Offer.class);
         query.setParameter("tag", tag);
-        return (List<Offer>) query.getResultList();
+        try {
+            offers = query.getResultList();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return offers;
     }
 
     @Override
     public List<Offer> findAvailable() {
-        Query query = entityManager.createQuery(DatabaseQuery.FIND_AVAILABLE_OFFERS);
-        return (List<Offer>) query.getResultList();
+        List<Offer> offers = new ArrayList<>();
+        TypedQuery<Offer> query = entityManager.createQuery(DatabaseQuery.FIND_AVAILABLE_OFFERS, Offer.class);
+        try {
+            offers = query.getResultList();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return offers;
     }
 
     @Override
     public List<Offer> getByPrice(Double firstValue, Double secondValue) {
-        Query query = entityManager.createQuery(DatabaseQuery.FIND_OFFERS_BY_PRICE);
+        List<Offer> offers = new ArrayList<>();
+        TypedQuery<Offer> query = entityManager.createQuery(DatabaseQuery.FIND_OFFERS_BY_PRICE, Offer.class);
         query.setParameter("firstValue", firstValue);
         query.setParameter("secondValue", secondValue);
-        return (List<Offer>) query.getResultList();
+        try {
+            offers = query.getResultList();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return offers;
     }
 }
