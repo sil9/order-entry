26586f6750aa143df9bbf317ecf62dfd3686dbb3
sil9 <sil9tyt@gmail.com>
2017-12-13 04:30:36 +0200

    add eureka client in microservices

--- manager/src/main/java/com/netcracker/selyutin/ManagerApplication.java
+++ manager/src/main/java/com/netcracker/selyutin/ManagerApplication.java
@@ -3,8 +3,10 @@ package com.netcracker.selyutin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ManagerApplication {
 
     public static void main(String[] args) {
