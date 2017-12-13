14271342ab49ef2f39984b4a5a7047778511ca53
sil9 <sil9tyt@gmail.com>
2017-12-13 04:17:42 +0200

    rename runner classes

--- manager/src/main/java/com/netcracker/selyutin/ManagerApplication.java
+++ manager/src/main/java/com/netcracker/selyutin/ManagerApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class ManagerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(ManagerApplication.class, args);
     }
 
 }
